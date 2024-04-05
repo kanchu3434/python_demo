e588c3b5ed2dab2f389acd295c498a98f568044d
kanchan <kanchankotkar001@gmail.com>
2024-04-05 21:58:27 +0530

    second password validation

--- passvalidation.py
+++ passvalidation.py
@@ -1,12 +1,13 @@
 import re
-password =input("enter password")
-reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,8}"
-pat= re.compile(reg)
-mat = re.search(pat,password)
-if mat:
+def main():
+ password = 'kanchan123@'
+ reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,8}"
+ pat= re.compile(reg)
+ mat = re.search(pat,password)
+ if mat:
     print("password is valid")
-else:
+ else:
     print("Invalid password")
-if __name__ =='_main_':
-    main()
+ if __name__ =='__main__':
+   main()
 
