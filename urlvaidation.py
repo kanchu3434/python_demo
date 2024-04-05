61de6db569f37e6d2bcbe25c0fc05d578661ce72
kanchan <kanchankotkar001@gmail.com>
2024-04-05 22:18:41 +0530

    URL validation using regex

--- urlvaidation.py
+++ urlvaidation.py
@@ -1,25 +1,8 @@
 import re
-
-# Define a function for
-# for validating an Email
-def check(s):
-	pat = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
-	if re.match(pat,s):
-		print("Valid Email")
-	else:
-		print("Invalid Email")
-
-# Driver Code
-if __name__ == '__main__':
-
-	# Enter the email
-	email = "ankitrai326@gmail.com"
-
-	# calling run function
-	check(email)
-
-	email = "my.ownsite@our-earth.org"
-	check(email)
-
-	email = "ankitrai326.com"
-	check(email)
+s = 'https://www.amazon.in/software-engeenering----/dp/0071240837'
+obj1=re.findall('\w+://',s)
+print(obj1)
+obj2 = re.findall('://([w_\.]+)',s)
+print(obj2)
+obj3 = re.findall('://([\w\_\.]+)(:(\d+))?',s)
+print(obj3)
